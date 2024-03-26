d8880bb6aac16f8a25f8c6757a2f8c2c12c78644
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-26 11:09:34 +1100

    form input reset

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import { Display } from "./Display";
 
+const initialState = "";
 export const Form = ({ getNewUserName }) => {
-  const [name, setName] = useState("");
-  const handleOnChnage = (e) => {
+  const [name, setName] = useState(initialState);
+  const handleOnChange = (e) => {
     const { value } = e.target;
     setName(value);
   };
   const handleOnSubmit = (e) => {
     e.preventDefault();
     getNewUserName(name);
+    setName(initialState);
   };
   return (
     <div>
       <Display name={name} />
       <form action="" onSubmit={handleOnSubmit}>
-        <input type="text" onChange={handleOnChnage} />
+        <input value={name} type="text" onChange={handleOnChange} />
         <button type="submit">Add User</button>
       </form>
     </div>
